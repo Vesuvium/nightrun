6334fc287ae702287b776490166505a0fdd86d46
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-08-17 23:25:11 +0200

    lint/nightrun: remove .js from imports

--- src/Nightrun.js
+++ src/Nightrun.js
@@ -1,8 +1,10 @@
-import Item from './Item.js';
-import Monster from './Monster.js';
-import Scene from './Scene.js';
-import Sprite from './Sprite.js';
-import LoadingBar from './ui/LoadingBar.js';
-import Text from './ui/Text.js';
+import Item from './Item';
+import Monster from './Monster';
+import Scene from './Scene';
+import Sprite from './Sprite';
+import LoadingBar from './ui/LoadingBar';
+import Text from './ui/Text';
 
-export { Item, Monster, LoadingBar, Scene, Sprite, Text };
+export {
+    Item, Monster, LoadingBar, Scene, Sprite, Text
+};
