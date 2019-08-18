41c3fbd5299c49b96f6b2e97ba8419f193efd449
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-08-18 17:24:53 +0200

    feat/ui/button: add constructor

--- src/ui/Button.js
+++ src/ui/Button.js
@@ -2,6 +2,9 @@ import Text from './Text';
 
 
 class Button extends Text {
+    constructor(phaser, text, x, y, style) {
+        super(phaser, text, x, y, style, { interactive: true });
+    }
 }
 
 
--- tests/unit/ui/Button.js
+++ tests/unit/ui/Button.js
@@ -5,6 +5,22 @@ import Button from '../../../src/ui/Button';
 import Text from '../../../src/ui/Text';
 
 
+test.beforeEach((ava) => {
+    ava.context.button = new Button('phaser', 'text', 'x', 'y', 'style');
+});
+
+
 test('the Button class', (ava) => {
     ava.true(Button.prototype instanceof Text);
+});
+
+
+test('the Button constructor', (ava) => {
+    const button = ava.context.button;
+    ava.is(button.phaser, 'phaser');
+    ava.is(button.text, 'text');
+    ava.is(button.x, 'x');
+    ava.is(button.y, 'y');
+    ava.is(button.style, 'style');
+    ava.deepEqual(button.options, { interactive: true });
 });
